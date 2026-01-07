a19a6677e0f646dd9d7f8d516eb1f5c27a027b45
ktkaytonheng <ktkaytonheng@bytedance.com>
2026-01-07 15:40:59 +0800

    modified colours and added to config/variables

--- scripts/generate-scss-vars.js
+++ scripts/generate-scss-vars.js
@@ -7,4 +7,14 @@ const scss = Object.entries(variablesJson)
     .map(([key, value]) => `$${key}: ${value};`)
     .join('\n');
 
-fs.writeFileSync('./src/styles/_variables.scss', scss);
+const ts = Object.entries(variablesJson)
+    .map(([key, value]) => `export const ${key.replaceAll('-', '_').toUpperCase()} = "${value}";`)
+    .join('\n');
+
+const warningComment =
+    '// The values in this file is retrieved from ./config/variables.json, generated via ./scripts/generate-scss-vars.js, DO NOT EDIT\n';
+
+fs.writeFileSync('./src/styles/_variables.scss', warningComment);
+fs.appendFileSync('./src/styles/_variables.scss', scss);
+fs.writeFileSync('./src/constants/variables.ts', warningComment);
+fs.appendFileSync('./src/constants/variables.ts', ts);
--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -1,8 +1,11 @@
-import variables from '~/config/variables.json';
-
-export const PADDING_X = variables['padding-x'];
-export const PADDING_Y = variables['padding-y'];
-export const TOPBAR_HEIGHT = variables['topbar-height'];
-export const SIDEBAR_WIDTH = variables['sidebar-width'];
-
-export default variables;
+// The values in this file is retrieved from ./config/variables.json, generated via ./scripts/generate-scss-vars.js, DO NOT EDIT
+export const PADDING_X = "20px";
+export const PADDING_Y = "16px";
+export const TOPBAR_HEIGHT = "64px";
+export const SIDEBAR_WIDTH = "240px";
+export const PLACEHOLDER_COLOR_BG_1 = "#37353E";
+export const PLACEHOLDER_COLOR_BG_2 = "#44444E";
+export const PLACEHOLDER_COLOR_BG_3 = "#715A5A";
+export const PLACEHOLDER_COLOR_TRIM_1 = "#D3DAD9";
+export const PLACEHOLDER_COLOR_TRIM_2 = "#C4ACA8";
+export const PLACEHOLDER_COLOR_TRIM_3 = "#B8A09A";
